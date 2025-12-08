26865c6acd593af47fff321a41bff57b23396dd3
akmalgt28 <deregen882@gmail.com>
2025-12-08 19:57:45 +0700

    Menambahkan Dashboard User

--- apps/dashboard.php
+++ apps/dashboard.php
@@ -4,21 +4,127 @@ $pageTitle = 'Dashboard';
 require __DIR__ . '/../templates/dashboard/header.php';
 ?>
 
-<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+<div class="w-full px-10 pt-4">
+  <h3 class="font-bold text-2xl text-textcolor dark:text-textcolor-dark">Selamat Datang, Mamal</h3>
+  <p class="mt-6 text-xl text-textcolor dark:text-textcolor-dark"><i class="fa-solid fa-fire text-brand-500 drop-shadow-[0_0_10px_theme(colors.brand.500)]"></i> Streak: 1 Hari</p>
+  <p class="mt-6 text-xl text-textcolor dark:text-textcolor-dark"><i class="fa-solid fa-fire text-gray-500"></i> Streak Mati ● Segera Mencatat</p>
 
-  <div class="bg-[#151936] border border-[#232862] rounded-xl p-6">
-    <h3 class="text-sm text-[#9AA0FF] mb-2">Total Users</h3>
-    <p class="text-2xl font-bold">1,245</p>
+<!-- 4 Grid Atas -->
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-6 mt-6">
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+  </div>
+<!-- Rekomendasi belajar -->
+  <div class="grid grid-cols-1 gap-6 mb-6 mt-6">
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-40 flex flex-col justify-center">
+      <p class="font-semibold text-textcolor dark:text-textcolor-dark text-sm sm:text-lg mb-3">Rekomendasi belajar</p>
+      <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Waktu Terbaik Belajar: </p>
+      <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base mb-3">Hari Paling Produktif</p>
+      <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-sm">Rekomendasi ini dihasilkan dari pola belajar Anda. Perbarui secara otomatis setiap ada sesi belajar baru.</p>
+    </div>
+  </div>
+<!-- 4 Grid Lanjutan -->
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-6 mt-6">
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-24 flex items-center gap-4">
+      <div class="w-14 h-14 border border-brand-500 bg-brand-500/30 rounded-xl flex items-center justify-center shrink-0">
+        <i class="fa-solid fa-pen text-textcolor dark:text-textcolor-dark"></i>
+      </div>
+      <div class="flex flex-col">
+        <p class="text-textcolor dark:text-textcolor-dark text-sm sm:text-base">Catatan Hari Ini</p>
+        <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-2xl sm:text-3xl">0</h3>
+      </div>
+    </div>
   </div>
 
-  <div class="bg-[#151936] border border-[#232862] rounded-xl p-6">
-    <h3 class="text-sm text-[#9AA0FF] mb-2">Total Notes</h3>
-    <p class="text-2xl font-bold">8,412</p>
+  <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-64 gap-4">
+      <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-xl">Grafik Belajar 7 Hari</h3>
+    </div>
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-64 gap-4">
+      <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-xl">Recent Session</h3>
+      <div class="py-4 border-b">
+        <p class="font-medium text-textcolor dark:text-textcolor-dark">Pemrograman Dasar</p>
+        <p class="text-sm text-gray-500 dark:text-textcolor-muted-dark">8 menit • 2025 12 01 05:59:11</p>
+      </div>
+      <div class="py-3 border-b">
+        <p class="font-medium text-textcolor dark:text-textcolor-dark">Pemrograman Dasar</p>
+        <p class="text-sm text-gray-500 dark:text-textcolor-muted-dark">8 menit • 2025 12 01 05:59:11</p>
+      </div>
+      <div class="py-3 border-b">
+        <p class="font-medium text-textcolor dark:text-textcolor-dark">Pemrograman Dasar</p>
+        <p class="text-sm text-gray-500 dark:text-textcolor-muted-dark">8 menit • 2025 12 01 05:59:11</p>
+      </div>
+    </div>
   </div>
-
-  <div class="bg-[#151936] border border-[#232862] rounded-xl p-6">
-    <h3 class="text-sm text-[#9AA0FF] mb-2">Active Streak</h3>
-    <p class="text-2xl font-bold">345</p>
+  <div class="grid grid-cols-1 gap-6 mb-6 mt-6">
+    <div class="bg-card dark:bg-card-dark border border-bordercolor dark:border-bordercolor-dark rounded-xl p-6 min-h-40 flex flex-col">
+      <h3 class="text-textcolor dark:text-textcolor-dark font-semibold text-xl">Recent Notes</h3>
+      <div class="flex flex-1 flex-col items-center justify-center text-center gap-2">
+        <i class="fa-regular fa-note-sticky text-2xl text-gray-400"></i>
+        <p class="font-medium text-gray-700 dark:text-textcolor-muted-dark">Belum Ada Catatan</p>
+        <p class="text-sm text-gray-400 dark:text-textcolor-muted-dark">Buat catatan baru dari menu Notes atau YouTube Editor</p>
+      </div>
+    </div>
   </div>
-
 </div>
